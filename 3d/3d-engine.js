f7a46ad39f5919c2fe9ff66e4255fcbd33fefcea
Tim Wilde <tim@midnightcoder.net>
2014-12-04 03:46:43 +0000

    Simple optimisations and perspective projection fix.

--- 3d/3d-engine.js
+++ 3d/3d-engine.js
@@ -1,22 +1,11 @@
 "use strict"
 
-var identity = [[1,0,0,0],
-                [0,1,0,0],
-                [0,0,1,0],
-                [0,0,0,1]];
-
-var camera = [0, 0, 150];
-var light = [100,-50,150];
-var normalSize = 15.0;
-
-var drawing = false;
-var red = 127, green = 0, blue = 0;
-
 var canv = document.getElementById('myCanvas');
 var out = document.getElementById('out');
 var modelSelect = document.getElementById('model');
 var showNormals = document.getElementById('normals');
 var showEdges = document.getElementById('showEdges');
+var polyCount = document.getElementById('count');
 var info = document.getElementById('info');
 var rotX = document.getElementById('rotX');
 var rotY = document.getElementById('rotY');
@@ -24,6 +13,17 @@ var rotZ = document.getElementById('rotZ');
 var xRot = document.getElementById('xRot');
 var yRot = document.getElementById('yRot');
 var zRot = document.getElementById('zRot');
+
+var identity = [[1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0],
+                [0,0,0,1]];
+
+var camera = [0, 0, canv.width/2];
+var light = [1000,-500,1500];
+var normalSize = 30.0;
+
+var red = 127, green = 0, blue = 0;
 
 var getRadians = function(degrees){
    return degrees * (Math.PI / 180);
@@ -80,15 +80,23 @@ var transform = function(points, matrix){
    return output;
 };
 
-var project = function(shape){
+var project = function(shape, scale){
    var output = [];
+   var aspect = 1.0;
+
+   scale = scale || 50;
 
    for(var v in shape){
       var vertex = shape[v];
       output[v] = [];
 
-      output[v][0] = camera[2] * (vertex[0]-camera[0]) / (camera[2] + vertex[2]) + camera[0]; // X
-      output[v][1] = camera[2] * (vertex[1]-camera[1]) / (camera[2] + vertex[2]) + camera[1]; // Y
+      var x = vertex[0];
+      var y = vertex[1];
+      var z = vertex[2];
+
+      output[v][0] = scale * (vertex[0] + camera[0]) / (camera[2] - vertex[2]); // X
+      output[v][1] = scale * (vertex[1] + camera[1]) / (camera[2] - vertex[2]); // Y
+      output[v][2] = scale * vertex[2];
    }
 
    return output;
@@ -110,21 +118,20 @@ var normalise = function(v){
    return output;
 };
 
-var calculateNormal = function(points, triangle){
-   var u = [points[triangle[1]][0] - points[triangle[0]][0],
+var crossProduct = function(points, triangle){
+   var v = [points[triangle[1]][0] - points[triangle[0]][0],
             points[triangle[1]][1] - points[triangle[0]][1],
             points[triangle[1]][2] - points[triangle[0]][2]];
 
-   var v = [points[triangle[2]][0] - points[triangle[0]][0],
+   var w = [points[triangle[2]][0] - points[triangle[0]][0],
             points[triangle[2]][1] - points[triangle[0]][1],
             points[triangle[2]][2] - points[triangle[0]][2]];
 
-   // Calculate cross product.
-   var normal = [(u[1] * v[2]) - (u[2] * v[1]),
-                 (u[2] * v[0]) - (u[0] * v[2]),
-                 (u[0] * v[1]) - (u[1] * v[0])];
+   var normal = [(v[1] * w[2]) - (v[2] * w[1]),
+                 (v[2] * w[0]) - (v[0] * w[2]),
+                 (v[0] * w[1]) - (v[1] * w[0])];
 
-   return normalise(normal);
+   return normal;
 };
 
 var dot = function(l,r){
@@ -176,18 +183,20 @@ var phong = function(normal, triangle, points){
 
 var plotObject = function(original, shape, object, xOff, yOff, scale, ctx){
    var polysDrawn = 0;
+   var lightVertex = normalise(light);
+   var solid = fill();
+   var wireframe = !solid;
 
    for(var i = 0; i < object.triangles.length; i++){
       var t = object.triangles[i];
 
-      var normal = calculateNormal(original, t);
-      var camVertex = normalise(camera);
-      var camAngle = camVertex[0] * normal[0] + camVertex[1] * normal[1] + camVertex[2] * normal[2];
+      var projectionNormal = normalise(crossProduct(shape, t));
+      var normal = normalise(crossProduct(original, t));
+      var visible = projectionNormal[2] >= 0;
 
-      if(camAngle > 0 || !fill()) {
+      if(visible || wireframe) {
          polysDrawn++;
 
-         var lightVertex = normalise(light);
          var lightIntensity = dot(normal, lightVertex);
          lightIntensity = lightIntensity < 0 ? 0 : lightIntensity;
 
@@ -200,25 +209,25 @@ var plotObject = function(original, shape, object, xOff, yOff, scale, ctx){
          }
 
          ctx.closePath();
-         if(fill()){
+         if(solid){
             ctx.fillStyle = 'rgb(' + (red + phong(normal, t, original)).toFixed(0) + ','
                                 + (green + phong(normal, t, original)).toFixed(0) + ','
                                 + (blue + phong(normal, t, original)).toFixed(0) + ')';
             ctx.fill();
          }
 
-         ctx.strokeStyle = (showEdges.checked || !fill()) ? ((camAngle > 0) ? '#900' : '#400') : ctx.fillStyle;
+         ctx.strokeStyle = (showEdges.checked || wireframe) ? (visible ? '#900' : '#400') : ctx.fillStyle;
          ctx.stroke();
 
          if(showNormals.checked){
-            var nXs = camera[2] * (object.normals[i][0] - camera[0]) / (camera[2] + object.normals[i][2]) + camera[0];
-            var nYs = camera[2] * (object.normals[i][1] - camera[1]) / (camera[2] + object.normals[i][2]) + camera[1];
+            var nXs = object.scale * (object.normals[i][0] - camera[0]) / (camera[2] + object.normals[i][2]);
+            var nYs = object.scale * (object.normals[i][1] - camera[1]) / (camera[2] + object.normals[i][2]);
 
-            var nXf = (camera[2] * ((object.normals[i][0] + ((normal[0] / scale) * normalSize)) - camera[0])) / (camera[2] + (object.normals[i][2] + ((normal[2] / scale) * normalSize))) + camera[0];
-            var nYf = (camera[2] * ((object.normals[i][1] + ((normal[1] / scale) * normalSize)) - camera[1])) / (camera[2] + (object.normals[i][2] + ((normal[2] / scale) * normalSize))) + camera[1];
+            var nXf = (object.scale * (object.normals[i][0] + (normal[0] / object.scale) * normalSize)) / (camera[2] + (object.normals[i][2] + ((normal[2] / object.scale) * normalSize)));
+            var nYf = (object.scale * (object.normals[i][1] + (normal[1] / object.scale) * normalSize)) / (camera[2] + (object.normals[i][2] + ((normal[2] / object.scale) * normalSize)));
 
             ctx.beginPath();
-            ctx.strokeStyle = camAngle > 0 ? '#0b0' : '#050';
+            ctx.strokeStyle = visible ? '#0b0' : '#050';
             ctx.lineWidth = 3;
             ctx.moveTo(xOff + (nXs * scale), yOff + (nYs * scale));
             ctx.lineTo(xOff + (nXf * scale), yOff + (nYf * scale));
@@ -227,7 +236,7 @@ var plotObject = function(original, shape, object, xOff, yOff, scale, ctx){
       }
    }
 
-   document.getElementById('count').innerText = ' (' +polysDrawn + ' rendered)';
+   polyCount.innerText = ' (' + polysDrawn + ' rendered)';
 };
 
 var sortByZIndex = function(transformedPoints, object){
@@ -272,7 +281,7 @@ var plotShape = function(obj, ctx){
    if(showNormals.checked)
       calculateCenter(transformedPoints, obj);
 
-   var twoDPoints = project(transformedPoints);
+   var twoDPoints = project(transformedPoints, obj.scale);
    plotObject(transformedPoints, twoDPoints, obj, 240, 240, 120, ctx);
    var twoDLight = project(light);
    ctx.FillStyle = '#fff';
@@ -281,7 +290,7 @@ var plotShape = function(obj, ctx){
 
 var clearCanvas = function(ctx){
    ctx.fillStyle = '#000';
-   ctx.clearRect(0, 0, 480, 480);
+   ctx.clearRect(0, 0, canv.width, canv.height);
 };
 
 var fixIndexing = function(obj){
@@ -359,7 +368,7 @@ var calculateScale = function(object){
          maxMagnitude = magnitude;
    }
 
-   object.scale = (camera[2] * Math.sin(getRadians(90))) / maxMagnitude;
+   object.scale = ((canv.width / 2) * Math.sin(getRadians(90))) / maxMagnitude;
 };
 
 var initialize = function(){
