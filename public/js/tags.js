e24bf3b9d2d3a2f851041cba06e7f62fdd59f85d
Tim Wilde <github@timwil.de>
2022-09-19 05:23:29 +0100

    Some a11y improvements and some colour.

---
+++ public/js/tags.js
@@ -0,0 +1,28 @@
+(async function (document) {
+   const response = await fetch('/tag-data.json')
+   const tags = (await response.json()).tags
+
+   const colors = chroma
+      .cubehelix()
+      .start(200)
+      .rotations(1)
+      .gamma(1.5)
+      .lightness([0.6, 0.8])
+      .scale()
+      .colors(tags.length);
+
+   const tagColors = {}
+   for (let index = 0; index < tags.length; index++) {
+      const background = chroma(colors[index]).saturate(2).alpha(0.2)
+      tagColors[tags[index]] = {
+         background: background,
+         border: background.darken()
+      }
+   }
+
+   const pageTags = document.querySelectorAll('[data-tag')
+   for (const tagEl of pageTags) {
+      tagEl.style.backgroundColor = tagColors[tagEl.dataset.tag].background
+      tagEl.style.borderColor = tagColors[tagEl.dataset.tag].border
+   }
+})(document)
--- tags.md
+++ tags.md
@@ -9,13 +9,13 @@ title: Tags
 <div class="page-tags">
    <ul class="tags">
       {%- for tag in allTags %}
-      <li class="tag"><a href="#{{ tag | slugify }}">{{tag | downcase}}</a></li>
+      <li class="tag"><a href="#{{ tag | slugify }}" data-tag="{{tag | slugify}}">{{tag | downcase}}</a></li>
       {%- endfor %}
    </ul>
 </div>
 
    {%- for tag in allTags %}
-<h3 id="{{tag | slugify}}">{{ tag | capitalize }}</h3>
+<h2 class="tag" id="{{tag | slugify}}" data-tag="{{tag | slugify}}">{{ tag | capitalize }}</h2>
 <ul>
       {%- for page in site.posts %}
          {%- if page.tags contains tag %}
