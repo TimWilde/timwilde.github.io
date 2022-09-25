6652709c19db1542ef984344db6159c2c0729c60
Tim Wilde <git@timwil.de>
2022-09-25 23:25:12 +0100

    Removed last remnant of Disqus.

--- public/js/disqus.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var d = document, s = d.createElement('script');
-    s.src = window.disqus.url + '/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
